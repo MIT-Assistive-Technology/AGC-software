3f72e1987748ac6abe4ffcca5a05e5fe45964023
AuroraH456 <101450189+AuroraH456@users.noreply.github.com>
2025-10-23 22:58:13 -0400

    continuous, controllable audio stream

--- src/voice_response/stream_audio.py
+++ src/voice_response/stream_audio.py
@@ -1,10 +1,13 @@
+# text-to-speech
+
 import numpy as np
 from piper import PiperVoice, SynthesisConfig
-import sounddevice as sd
+import sounddevice as sd # need to install
 import threading
+import queue
 
 # customize voice
-syn_config = SynthesisConfig(
+syn_config = SynthesisConfig( 
     volume=1.0,  # volume; default = 1.0
     length_scale=1.0,  # length of speech; < 1.0 means faster, > 1.0 means slower; default=1.0
     noise_scale=0.667,  # amount of generator noise; default=0.667
@@ -13,32 +16,87 @@ syn_config = SynthesisConfig(
     speaker_id=2 # chooses which speaker to use; relevant only for multi-speaker models 
 )
 
-stopPlay = False # if True, streaming will stop
+q = queue.Queue()
+leftover = []
 
-# plays speech.
-# voiceStr: voice ID of the chosen voice/language (default = english) 
-# text: the text to speak
-def piperContinuousStream(voiceStr = "en_US-lessac-medium.onnx", text = "this is a test. 1 2 3. 4 5 6. 7 8 9."):
-    global stopPlay # use global version of variable (so it can be changed by a function definition)
-    stopPlay = False
+def audio_callback(outdata, frames, time_info, status): # continuously feed audio data into stream
+    if status:
+        print(status)
+    
+    global leftover # leftover audio from long chunks
+    if len(leftover) != 0: # first priority: get from leftover
+        data = leftover.copy()
+        leftover = []
+    elif not q.empty(): # get from queue
+        data = q.get_nowait()
+    else: # no data anywhere -> silence
+        outdata[:] = np.zeros((frames, 1))
+        return
+
+    #print("I need " + str(frames) + " frames, and you are giving me " + str(len(data)))
+    
+    if len(data) < frames: # chunk too short -> pad with zeros
+        need = frames-len(data)
+        if q.empty(): # no more data -> zeroes
+            data = np.append(data, np.zeros(need))
+        else: # there is more data -> get more data to fill empty space
+            extraData = q.get_nowait()
+            assert(len(extraData) > frames) # is probably true, each chunk is pretty long
+            assert(len(leftover) == 0) # shouldn't write over anything
+            data = np.append(data, extraData[:need])
+            leftover = extraData[need:].copy()
+    elif len(data) > frames: # chunk too long -> truncate & put the rest in leftover
+        leftover = data[frames:].copy()
+        data = data[:frames].copy()
+    
+    assert len(data) == frames
+    outdata[:] = data.reshape(-1, 1).copy()
+
+
+def addAudio(text = "this is a test. 1 2 3. 4 5 6. 7 8 9."): # add audio to the end of queue
+    #voice = PiperVoice.load(voiceStr)
+    for chunk in voice.synthesize(text, syn_config=syn_config):
+        audio_data = np.frombuffer(chunk.audio_int16_bytes, dtype=np.int16).astype(np.float32) / 32768.0
+        q.put(audio_data)
+
+def startStream(): # initialize audio stream
+    global stream
+    stream = sd.OutputStream(samplerate=voice.config.sample_rate, channels=1, callback=audio_callback)
+    stream.start()
+    q.join()
+
+#ukrainian model: uk_UA-ukrainian_tts-medium.onnx
+def prepareVoice(voiceStr = "en_US-lessac-medium.onnx"): # initialize voice
+    global voice
     voice = PiperVoice.load(voiceStr)
-    with sd.OutputStream(samplerate=22050, channels=1, dtype='int16') as stream:
-        for chunk in voice.synthesize(text, syn_config=syn_config): # piper TTS parses text into chunks (sentences)
-            if stopPlay: # if stopPlay flag is set to True, stop playing
-                break
-            audio_data = np.frombuffer(chunk.audio_int16_bytes, dtype=np.int16) # chunk is converted to playable format
-            stream.write(audio_data) # plays chunk of audio
 
+def pauseStream(): # pause audio
+    stream.stop()
 
-# puts streaming in a thread so other processes can continue. Streaming will end if enter is pressed
-# voiceStr: voice ID of the chosen voice/language (default = english) 
-# text: the text to speak
-# does NOT support multiple calls of playAudio if one of the calls are interrupted
-# TODO: use callback() function to continuously feed text to the OutputStream. Hopefully the StopPlay variable becomes unnecessary
-def playAudio(voiceStr = "en_US-lessac-medium.onnx", text = "this is a test. 1 2 3. 4 5 6. 7 8 9."): 
-    thread = threading.Thread(target=piperContinuousStream,args=(voiceStr,text)) 
-    thread.start()
+def resumeStream(): # resumes audio
+    stream.start()
 
-    input("Press enter to stop\n") # TODO: replace with real way to check whether to stop
-    global stopPlay
-    stopPlay = True
+def clearStream(): # clear queued text
+    global leftover
+    leftover = []
+    while q.empty() == False:
+        q.get_nowait()
+
+def testCommands(): # test usable commands
+    print("Enter text to speak, 1 to stop, 2 to resume, 3 to clear queued text")
+    while True:
+        text = input("Enter selection: ")
+        if text == "1":
+            pauseStream()
+        elif text == "2":
+            resumeStream()
+        elif text == "3":
+            clearStream()
+        else:
+            addAudio(text)
+
+prepareVoice()
+addAudio("hello") # You need an initial text to make it keep working, trust
+threading.Thread(target=startStream).start()
+
+#testCommands() # uncomment to test usable commands
